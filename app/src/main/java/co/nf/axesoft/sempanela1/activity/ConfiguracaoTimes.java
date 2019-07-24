e9e07f26fd8a6851d2847faddf58bece129271e5
richard <richardmachado.93@gmail.com>
2019-07-24 13:41:06 -0300

    inserido botão "adicionar" na view principal

--- app/src/main/java/co/nf/axesoft/sempanela1/activity/ConfiguracaoTimes.java
+++ app/src/main/java/co/nf/axesoft/sempanela1/activity/ConfiguracaoTimes.java
@@ -3,12 +3,16 @@ package co.nf.axesoft.sempanela1.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Adapter;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -21,7 +25,11 @@ public class ConfiguracaoTimes extends AppCompatActivity {
     private ListView listView;
     private ArrayAdapter <String> adapter;
     private ArrayList<String> nomesJogadores;
+    private SharedPreferences preferences;
 
+    //Views para a entrada de nomes na lista de jogadores
+    private Button btnAddLista;
+    private EditText eTxtJogador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,13 +41,30 @@ public class ConfiguracaoTimes extends AppCompatActivity {
         toolbar.setTitle("Tira o time aí");
         setSupportActionBar(toolbar);
 
+
+        btnAddLista = findViewById(R.id.btnAddJogadores);
+        eTxtJogador = findViewById(R.id.eTxtAddNome);
+
+
         listView = findViewById(R.id.listViewJogadoresAdd);
 
         //Monta o adapter
         nomesJogadores = new ArrayList<>();
-        nomesJogadores.add("Melhor Jogador do mundo");
+        //nomesJogadores.add("Melhor Jogador do mundo");
         adapter =  new JogadorAdapter(getApplicationContext(),nomesJogadores);
         listView.setAdapter(adapter);
+
+
+        btnAddLista.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nomesJogadores.add(eTxtJogador.getText().toString());
+                adapter.notifyDataSetChanged();
+            }
+        });
+
+
+
 
 
     }
@@ -76,4 +101,31 @@ public class ConfiguracaoTimes extends AppCompatActivity {
         }
 
     }
+    private ArrayList<String> addNomesListJogadores(){
+
+
+
+        ArrayList<String> listJogadores = new ArrayList<>();
+
+        listJogadores.add("Ronaldinho");
+        listJogadores.add("Romero");
+        listJogadores.add("Ronaldo Fenomeno");
+        listJogadores.add("Cristiano Ronaldo");
+        listJogadores.add("Neymar");
+        listJogadores.add("Camisa 10");
+        listJogadores.add("Perna de Pau");
+        listJogadores.add("Joga muito");
+        listJogadores.add("Capitão");
+        listJogadores.add("Mala");
+        listJogadores.add("Completa time");
+        listJogadores.add("Joga fácil");
+        listJogadores.add("Sem mundial");
+        listJogadores.add("Chorão");
+        listJogadores.add("só reclama");
+
+        return listJogadores;
+
+
+    }
+
 }
